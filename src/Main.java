02ffff5a73b97f8af3eb0d338babe176d5394b40
Daria Florczyk <dariaflorczyk94@gmail.com>
2024-01-16 20:58:14 +0100

    OBWOD + POLE PROSTOKATA

--- src/Main.java
+++ src/Main.java
@@ -4,24 +4,19 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("WZORY MATEMATYCZNE:");
         Scanner sc = new Scanner(System.in);
-//   System.out.println("Podaj dlugosc boku kwadratu aby wyliczyc jego objetosc:");
-//   Scanner sc = new Scanner(System.in);
-// MOZLIWOSC EDYTOWANIA KONSOLI -->       int sideLenghA = sc.nextInt();
-//   System.out.println("Obwod kwadratu to: " + (4 * sideLengh));
-//   System.out.println("Obwod prostokata: " + (2 * sideLenghA + 2 * sideLenghB));
-//   System.out.println("Podaj dlugosc boku A prostokata: ");
-//        double sideLenghA = sc.nextDouble();
-//  System.out.println("Podaj dlugosc boku B prostokata: ");
-//        int sideLenghB = sc.nextInt();
-// System.out.println("Pole prostokata: " + (sideLenghA * sideLenghB));
-System.out.println("Podaj dlugosc boku A prostokata: ");
-    int sideLenghA = sc.nextInt();
-System.out.println("Podaj dlugosc boku B prostokata: ");
-    int sideLenghB = sc.nextInt();
-        System.out.println("Czy A == B? " + (sideLenghA == sideLenghB));
-
-
-
+        System.out.println("Podaj dlugosc boku kwadratu aby wyliczyc jego obwod:");
+        int sideLengh = sc.nextInt(); // MOZLIWOSC EDYTOWANIA KONSOLI
+        System.out.println("Obwod kwadratu to: " + (4 * sideLengh));
+        System.out.print("Podaj dlugosc boku A prostokata: ");
+        int sideLenghA = sc.nextInt();
+        System.out.print("Podaj dlugosc boku B prostokata: ");
+        int sideLenghB = sc.nextInt();
+        System.out.println("Obwod prostokata: " + (2 * sideLenghA + 2 * sideLenghB));
+        System.out.print("Podaj dlugosc boku A prostokata: ");
+        double sideLenghAA = sc.nextDouble();
+        System.out.print("Podaj dlugosc boku B prostokata: ");
+        double sideLenghBB = sc.nextDouble();
+ System.out.println("Pole prostokata: " + (sideLenghAA * sideLenghBB));
     }
  }
 
