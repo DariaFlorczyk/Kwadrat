5eb0571181ba339369058370e508d46189f247dc
Daria Florczyk <dariaflorczyk94@gmail.com>
2024-01-16 20:43:07 +0100

    Program na obwod prostokata

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,22 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 //   System.out.println("Podaj dlugosc boku kwadratu aby wyliczyc jego objetosc:");
 //   Scanner sc = new Scanner(System.in);
-//   int sideLenghA = sc.nextInt();
+// MOZLIWOSC EDYTOWANIA KONSOLI -->       int sideLenghA = sc.nextInt();
 //   System.out.println("Obwod kwadratu to: " + (4 * sideLengh));
-    System.out.println("Podaj dlugosc boku A prostokata:");
-        int sideLenghA = sc.nextInt();
-    System.out.println("Podaj dlugosc boku B prostokata:");
-        int sideLenghB = sc.nextInt();
-    System.out.println("Obwod prostokata: " + (2 * sideLenghA + 2 * sideLenghB));
-}
+//   System.out.println("Obwod prostokata: " + (2 * sideLenghA + 2 * sideLenghB));
+//   System.out.println("Podaj dlugosc boku A prostokata: ");
+//        double sideLenghA = sc.nextDouble();
+//  System.out.println("Podaj dlugosc boku B prostokata: ");
+//        int sideLenghB = sc.nextInt();
+// System.out.println("Pole prostokata: " + (sideLenghA * sideLenghB));
+System.out.println("Podaj dlugosc boku A prostokata: ");
+    int sideLenghA = sc.nextInt();
+System.out.println("Podaj dlugosc boku B prostokata: ");
+    int sideLenghB = sc.nextInt();
+        System.out.println("Czy A == B? " + (sideLenghA == sideLenghB));
+
+
+
+    }
  }
 
